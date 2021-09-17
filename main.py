bc2dc0b828ceed7e2b58027b9dbd225fd8b7905d
aymankhalaf-imagry <a.khalaf@imagry.co>
2021-09-18 00:29:48 +0300

    extract functions and imports from wetdry_features function

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from wetdry_curves_features import wetdry_features
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    wetdry_features("""{
+  "grower_id": "grower_1",
+  "plot_id": "plot_1",
+  "iplant_id": "iplant_1",
+  "soil_moisture_sensor": "4025__VWC_Mineral_Soil"
+    }""", "test-data.csv", ".", 1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
